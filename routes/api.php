e226e14510590d1d66fe7846237690389d6fe004
Ygor-Machado <ygaounderground011@gmail.com>
2024-10-22 22:04:21 -0300

    feat: adiciona rotas e controller do front

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,5 +16,15 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('products', ProductController::class);
 });
+
+Route::controller(FrontController::class)->prefix('frontend')->group(function () {
+    Route::get('buildmenu', 'buildMenu');
+    Route::get('/home', 'home');
+    Route::get('/products', 'products');
+    Route::get('/productAssessories', 'productAssessories');
+    Route::get('/product/{product}', 'product');
+    Route::get('/product', 'static');
+});
+
 
 require __DIR__.'/auth.php';
